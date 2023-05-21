2c714ef7db91467cb3e7a7212364aa66f3cb1dcf
Gabriel Maco <21101344@ue.edu.pe>
2023-05-20 19:21:15 -0500

    Agregar archivos de proyecto.

---
+++ CASO02GMACO.API/Data/Parcial202321101344Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CASO02GMACO.API.Data;
+
+public partial class Parcial202321101344Context : DbContext
+{
+    public Parcial202321101344Context()
+    {
+    }
+
+    public Parcial202321101344Context(DbContextOptions<Parcial202321101344Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Team> Team { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=WS1602499;Database=Parcial202321101344;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Team>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Country).HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CASO02GMACO.API/Data/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CASO02GMACO.API.Data;
+
+public partial class Team
+{
+    public string? Description { get; set; }
+
+    public string? Country { get; set; }
+
+    public int Id { get; set; }
+}
